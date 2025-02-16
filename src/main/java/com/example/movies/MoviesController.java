7fd9079e5049fe82e69d1349d1789b57cf43cb65
nupurtevatiya96 <100755419+nupurtevatiya96@users.noreply.github.com>
2025-02-16 20:07:27 +0530

    Implemented controller, repository and service for movie collection

--- src/main/java/com/example/movies/MoviesController.java
+++ src/main/java/com/example/movies/MoviesController.java
@@ -2,26 +2,32 @@ package com.example.movies;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-
-
-
-
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
+@RequestMapping("/api/v1/movies")
 public class MoviesController {
 
-    @GetMapping("/path")
-    public String getMethodName() {
-        return new String("hello");
-    }
-    
+    @Autowired
+    private MovieService movieService;
 
-    @RequestMapping("/")
-    public String movies() {
-        return "Hello world";
+    @GetMapping
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
-    
-    
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable String imdbId) {
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(imdbId), HttpStatus.OK);
+    }
+
 }
