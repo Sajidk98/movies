3bfcfdb5acdb2e2c0b610152995811d41b89b7d3
sajidk98 <sajidansari18@gmail.com>
2025-02-18 01:11:29 +0530

    implemented post review

--- src/main/java/com/example/movies/Review.java
+++ src/main/java/com/example/movies/Review.java
@@ -4,9 +4,21 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Document(collection = "review")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Review {
     @Id
     private ObjectId id;
+
     private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
 }
